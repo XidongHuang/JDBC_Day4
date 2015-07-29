7cd82c83b6568e63962bef038f76e17164d91124
tony <tony@XPS>
2015-07-29 16:42:12 -0400

    JDBC-- DBCP,BasicDataSourceFactory

--- src/tony/java/jdbc/JDBCTest.java
+++ src/tony/java/jdbc/JDBCTest.java
@@ -1,10 +1,14 @@
 package tony.java.jdbc;
 
-import static org.junit.Assert.*;
 
+import java.lang.Thread;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLClientInfoException;
+import java.sql.SQLException;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.dbcp.BasicDataSourceFactory;
 import org.junit.Test;
 import org.omg.CORBA.Current;
 
@@ -13,153 +17,235 @@ import com.mysql.jdbc.interceptors.ServerStatusDiffInterceptor;
 import java.sql.PreparedStatement;
 
 import java.sql.Statement;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+
 
 public class JDBCTest {
 	static int count = 0;
+
+	/**
+	 * 1. 加载dbcp 的properties 配置文件: 配置文件中的键需要来自BasicDataSource的属性
+	 * 2. 调用 BasicDataSourceFactory 的createDataSrouce 方法创建DataSourceFactory
+	 * 的实例
+	 * 3. 从DataSource 实例中获取数据库链接
+	 * @throws Exception
+	 */
+	
 	@Test
-	public void testBatch(){
+	public void testDBCPWithDataSourceFactory() throws Exception {
+
+		Properties properties = new Properties();
+		InputStream inStream = JDBCTest.class.getClassLoader().getResourceAsStream("dbcp.properties");
+		properties.load(inStream);
+
+		DataSource dataSource = BasicDataSourceFactory.createDataSource(properties);
+		System.out.println(dataSource.getConnection());
+
+		BasicDataSource basicDataSource = (BasicDataSource)dataSource;
+		System.out.println(basicDataSource.getMaxWait());
 		
+	}
+
+	/**
+	 * 使用DBCP 数据库连接池 1. 加入jar 包 pool-15.5, DBCP 1.4 2. 创建数据库连接池 3. 创建 DBCP 数据源实例
+	 * 4. 从数据源中获取数据库链接
+	 * 
+	 * @throws SQLException
+	 */
+
+	@Test
+	public void testDBCP() throws SQLException {
+
+		// 创建 DBCP 数据源实例
+	
+		final BasicDataSource dataSource = new BasicDataSource();
+
+		// 2. 为数据源实例制定必须的属性
+		dataSource.setUsername("root");
+		dataSource.setPassword("12345678");
+		dataSource.setUrl("jdbc:mysql://testdb.c9xqmbjdijez.us-west-2.rds.amazonaws.com:3306/testDB");
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+
+		// 3. 指定数据源的一些可选的属性
+		// 3.1 指定数据库连接池中初始化连接数的个数
+		dataSource.setInitialSize(5);
+
+		// 3.2 指定最大的连接数: 同意时刻可以同时向数据库申请的链接数
+		dataSource.setMaxActive(5);
+
+		// 3.3 指定最小连接数: 在数据库连接池空闲状态下，连接池中最少有多少个链接
+		dataSource.setMinIdle(2);
+
+		// 3.4 等待数据库连接池分配链接的最长时间，单位为毫秒。超出该时间将抛出异常
+		dataSource.setMaxWait(1000 * 5);
+
+		// 4. 从数据源中获取数据库链接
+		Connection connection = dataSource.getConnection();
+		System.out.println(connection.getClass());
+
+		connection = dataSource.getConnection();
+		System.out.println(connection.getClass());
+		
+		connection = dataSource.getConnection();
+		System.out.println(connection.getClass());
+		
+		connection = dataSource.getConnection();
+		System.out.println(connection.getClass());
+		
+		Connection connection2 = dataSource.getConnection();
+		System.out.println(">"+connection.getClass());
+		
+		new Thread(){
+			public void run(){
+				try {
+					Connection conn = dataSource.getConnection();
+					System.out.println(conn.getClass());
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			};
+			
+			
+		}.start();
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		connection2.close();
+	}
+
+	@Test
+	public void testBatch() {
+
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		String sql = null;
-		
-		
+
 		try {
-			
+
 			connection = JDBCTools.getConnection();
 			JDBCTools.beginTx(connection);
-			
+
 			sql = "INSERT INTO customers VALUES(?,?,?)";
 			preparedStatement = connection.prepareStatement(sql);
-			
+
 			long begin = System.currentTimeMillis();
-			for(int i = 0; i<100000;i++){
-				preparedStatement.setInt(1, i+1);
-				preparedStatement.setString(2, "name_"+i);
-				preparedStatement.setString(3, "location"+i);
+			for (int i = 0; i < 100000; i++) {
+				preparedStatement.setInt(1, i + 1);
+				preparedStatement.setString(2, "name_" + i);
+				preparedStatement.setString(3, "location" + i);
 				count++;
 				System.out.println(count);
-				//"积攒" SQL 
+				// "积攒" SQL
 				preparedStatement.addBatch();
-				
-				//当"积攒" 到一定程度，就统一的执行一次. 并且清空先前"积攒" 的SQL
-				if((i+1) %50000 == 0){
+
+				// 当"积攒" 到一定程度，就统一的执行一次. 并且清空先前"积攒" 的SQL
+				if ((i + 1) % 50000 == 0) {
 					preparedStatement.executeBatch();
 					preparedStatement.clearBatch();
 				}
 
-				
 			}
-			
-			//若总条数不是批量数值的整数倍，则还需要在额外地执行一次
-			if(10000 % 300 != 0){
+
+			// 若总条数不是批量数值的整数倍，则还需要在额外地执行一次
+			if (10000 % 300 != 0) {
 				preparedStatement.executeBatch();
 				preparedStatement.clearBatch();
 			}
-			
-			
+
 			long end = System.currentTimeMillis();
-			System.out.println("Time: " + (end - begin));//159898
+			System.out.println("Time: " + (end - begin));// 159898
 			JDBCTools.commit(connection);
-		} catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 			JDBCTools.roolback(connection);
 		} finally {
 			// TODO: handle finally clause
 			JDBCTools.releaseDB(null, preparedStatement, connection);
-		
+
 		}
-		
-		
-		
-		
+
 	}
-	
-	
-	
+
 	@Test
 	public void testBatchWithPreparedStatement() {
-//		static int count = 0;
+		// static int count = 0;
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		String sql = null;
-		
-		
+
 		try {
-			
+
 			connection = JDBCTools.getConnection();
 			JDBCTools.beginTx(connection);
 			sql = "INSERT INTO customers VALUES(?,?,?)";
 			preparedStatement = connection.prepareStatement(sql);
-			
+
 			long begin = System.currentTimeMillis();
-			for(int i = 0; i<1000;i++){
-				preparedStatement.setInt(1, i+1);
-				preparedStatement.setString(2, "name_"+i);
-				preparedStatement.setString(3, "location"+i);
+			for (int i = 0; i < 1000; i++) {
+				preparedStatement.setInt(1, i + 1);
+				preparedStatement.setString(2, "name_" + i);
+				preparedStatement.setString(3, "location" + i);
 				System.out.println(sql);
 				preparedStatement.executeUpdate();
 			}
 			long end = System.currentTimeMillis();
-			System.out.println("Time: " + (end - begin));//159898
+			System.out.println("Time: " + (end - begin));// 159898
 			JDBCTools.commit(connection);
-		} catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 			JDBCTools.roolback(connection);
 		} finally {
 			// TODO: handle finally clause
 			JDBCTools.releaseDB(null, preparedStatement, connection);
-		
+
 		}
-		
-		
-		
+
 	}
-	
-	
-	
-	
+
 	/**
-	 * 向Oracle 的Customer 数据表中插入10万条记录
-	 * 测试如何插入用时最短
-	 * 1. 使用Statement 	 
+	 * 向Oracle 的Customer 数据表中插入10万条记录 测试如何插入用时最短 1. 使用Statement
 	 */
-	
+
 	@Test
 	public void testBatchWithStatement() {
-		
+
 		Connection connection = null;
 		Statement statement = null;
 		String sql = null;
-		
-		
+
 		try {
-			
+
 			connection = JDBCTools.getConnection();
 			JDBCTools.beginTx(connection);
 			statement = connection.createStatement();
-			
+
 			long begin = System.currentTimeMillis();
-			for(int i = 0; i<1000;i++){
-				sql = "INSERT INTO customers VALUES("+(i+1)+",'name_"+i+"','location_"+i + "');";
+			for (int i = 0; i < 1000; i++) {
+				sql = "INSERT INTO customers VALUES(" + (i + 1) + ",'name_" + i + "','location_" + i + "');";
 				System.out.println(sql);
 				statement.executeUpdate(sql);
-				
+
 			}
 			long end = System.currentTimeMillis();
-			System.out.println("Time: " + (end - begin));//79257
+			System.out.println("Time: " + (end - begin));// 79257
 			JDBCTools.commit(connection);
-		} catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 			JDBCTools.roolback(connection);
 		} finally {
 			// TODO: handle finally clause
 			JDBCTools.releaseDB(null, statement, connection);
-		
+
 		}
-		
-		
-		
+
 	}
 
 }
--- src/tony/java/jdbc/JDBCTools.java
+++ src/tony/java/jdbc/JDBCTools.java
@@ -13,7 +13,7 @@ import java.util.Properties;
 public class JDBCTools {
 
 	// 处理数据库事务
-	//提交事务
+	// 提交事务
 	public static void commit(Connection connection) {
 
 		if (connection != null) {
@@ -30,7 +30,7 @@ public class JDBCTools {
 
 	}
 
-	//回滚事务
+	// 回滚事务
 	public static void roolback(Connection connection) {
 
 		if (connection != null) {
@@ -46,35 +46,23 @@ public class JDBCTools {
 		}
 
 	}
-	
-	
-	//上交事务
-	public static void beginTx(Connection connection){
-		if(connection != null){
-			
+
+	// 上交事务
+	public static void beginTx(Connection connection) {
+		if (connection != null) {
+
 			try {
 				connection.setAutoCommit(false);
-				
+
 			} catch (Exception e) {
 				e.printStackTrace();
 				// TODO: handle exception
 			}
-			
-		
-		} 
-		
-		
-	}
-	
-	
 
-	/**
-	 * ִ�� SQL ���, ʹ�� PreparedStatement
-	 * 
-	 * @param sql
-	 * @param args:
-	 *            ��д SQL ռλ��Ŀɱ����
-	 */
+		}
+
+	}
+
 	public static void update(String sql, Object... args) {
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
@@ -96,41 +84,24 @@ public class JDBCTools {
 		}
 	}
 
-	/**
-	 * ִ�� SQL �ķ���
-	 * 
-	 * @param sql:
-	 *            insert, update �� delete�� ��� select
-	 */
 	public static void update(String sql) {
 		Connection connection = null;
 		Statement statement = null;
 
 		try {
-			// 1. ��ȡ��ݿ�����
 			connection = getConnection();
 
-			// 2. ���� Connection ����� createStatement() ������ȡ Statement ����
 			statement = connection.createStatement();
 
-			// 4. ���� SQL ���: ���� Statement ����� executeUpdate(sql) ����
 			statement.executeUpdate(sql);
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			// 5. �ر���ݿ���Դ: ��������ر�.
 			releaseDB(null, statement, connection);
 		}
 	}
 
-	/**
-	 * �ͷ���ݿ���Դ�ķ���
-	 * 
-	 * @param resultSet
-	 * @param statement
-	 * @param connection
-	 */
 	public static void releaseDB(ResultSet resultSet, Statement statement, Connection connection) {
 
 		if (resultSet != null) {
@@ -159,31 +130,19 @@ public class JDBCTools {
 
 	}
 
-	/**
-	 * ��ȡ��ݿ����ӵķ���
-	 */
 	public static Connection getConnection() throws IOException, ClassNotFoundException, SQLException {
-		// 0. ��ȡ jdbc.properties
-		/**
-		 * 1). �����ļ���Ӧ Java �е� Properties �� 2). ����ʹ������������� bin
-		 * Ŀ¼(��·����)���ļ�
-		 */
+
 		Properties properties = new Properties();
 		InputStream inStream = JDBCTools.class.getClassLoader().getResourceAsStream("jdbc.properties");
 		properties.load(inStream);
 
-		// 1. ׼����ȡ���ӵ� 4 ���ַ�: user, password, jdbcUrl, driverClass
 		String user = properties.getProperty("user");
 		String password = properties.getProperty("password");
 		String jdbcUrl = properties.getProperty("jdbcUrl");
 		String driverClass = properties.getProperty("driver");
 
-		// 2. ������: Class.forName(driverClass)
 		Class.forName(driverClass);
 
-		// 3. ����
-		// DriverManager.getConnection(jdbcUrl, user, password)
-		// ��ȡ��ݿ�����
 		Connection connection = DriverManager.getConnection(jdbcUrl, user, password);
 		return connection;
 	}
